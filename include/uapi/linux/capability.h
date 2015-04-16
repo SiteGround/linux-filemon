1edeb879c9a4522fc18ef36293ef0a8cb055c38a
root <root@kernighan.(none)>
2015-04-16 11:50:40 +0300

    include/uapi/linux/capability.h: Added CAP_LXC_ADMIN
    Signed-off-by: Nikolay Borisov <n.borisov@siteground.com>
    (cherry picked from commit 46c38700112af865042bba9910708c19608d0fed)

--- include/uapi/linux/capability.h
+++ include/uapi/linux/capability.h
@@ -351,8 +351,11 @@ struct vfs_cap_data {
 
 #define CAP_AUDIT_READ		37
 
+/* Allow admin privileges inside containers */
 
-#define CAP_LAST_CAP         CAP_AUDIT_READ
+#define CAP_LXC_ADMIN      38
+
+#define CAP_LAST_CAP         CAP_LXC_ADMIN
 
 #define cap_valid(x) ((x) >= 0 && (x) <= CAP_LAST_CAP)
 
